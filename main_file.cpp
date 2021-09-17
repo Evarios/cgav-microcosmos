eb15cd37a82c662eae953810e953d8e57db9c1d5
Evarios <jan.brozdziak@gmail.com>
2021-09-17 17:02:22 +0200

    working assimp, our model loaded

--- main_file.cpp
+++ main_file.cpp
@@ -23,15 +23,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <glm/gtc/matrix_transform.hpp>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+#include <iostream>
 #include "constants.h"
 #include "allmodels.h"
 #include "lodepng.h"
 #include "shaderprogram.h"
-
+#include "myCube.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 float speed_x = 0;//[radians/s]
 float speed_y = 0;//[radians/s]
-
+GLuint tex;
+std::vector<glm::vec4> verts;
+std::vector<glm::vec4> norms;
+std::vector<glm::vec2> texCoords;
+std::vector<unsigned int > indices;
 //Error processing callback procedure
 void error_callback(int error, const char* description) {
 	fputs(description, stderr);
@@ -69,42 +78,164 @@ void key_callback(
 	}
 }
 
+GLuint readTexture(const char* filename) {
+	GLuint tex;
+	glActiveTexture(GL_TEXTURE0);
+
+	//Read into computers memory
+	std::vector<unsigned char> image;   //Allocate memory 
+	unsigned width, height;   //Variables for image size
+	//Read the image
+	unsigned error = lodepng::decode(image, width, height, filename);
+
+	//Import to graphics card memory
+	glGenTextures(1, &tex); //Initialize one handle
+	glBindTexture(GL_TEXTURE_2D, tex); //Activate handle
+	//Copy image to graphics cards memory reprezented by the active handle
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, width, height, 0,
+		GL_RGBA, GL_UNSIGNED_BYTE, (unsigned char*)image.data());
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	return tex;
+}
+
+void loadModel(std::string plik) {
+	using namespace std;
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(plik, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenSmoothNormals);
+	cout << importer.GetErrorString() << endl;
+	
+	aiMesh *mesh = scene->mMeshes[0];
+	
+	for (int i = 0; i < mesh->mNumVertices; i++) {
+		aiVector3D vertex = mesh->mVertices[i]; //aiVector3D podobny do glm::vec3
+		verts.push_back(glm::vec4(vertex.x, vertex.y, vertex.z, 1));		
+		
+		aiVector3D normal = mesh->mNormals[i]; // wektory znormalizowane
+		norms.push_back(glm::vec4(normal.x, normal.y, normal.z, 0));
+
+		//0 to numer zestawu wsp. teksturowania
+		aiVector3D texCoord = mesh->mTextureCoords[0][i];
+		texCoords.push_back(glm::vec2(texCoord.x, texCoord.y));
+
+		//dla każdego wielokąta składowego
+		for (int i = 0; i < mesh->mNumFaces; i++) {
+			aiFace& face = mesh->mFaces[i]; //face to jeden z wielokątów siatki
+			//dla każdego indeksu->wierzchkołka tworzącego wielokąt
+			//dla aiProcess_Triangulate to zawsze będzie 3
+			for (int j = 0; j < face.mNumIndices; j++) {
+				indices.push_back(face.mIndices[j]);
+			}
+		}
+	}
+	return;
+}
+
 //Initialization code procedure
 void initOpenGLProgram(GLFWwindow* window) {
 	initShaders();
 	//************Place any code here that needs to be executed once, at the program start************
-    glClearColor(0, 0, 0, 1); //Set color buffer clear color
+	glClearColor(0, 0, 0, 1); //Set color buffer clear color
 	glEnable(GL_DEPTH_TEST); //Turn on pixel depth test based on depth buffer
 	glfwSetKeyCallback(window, key_callback);
+	tex = readTexture("bricks.png");
+	loadModel("body.model3D");
 }
 
 //Release resources allocated by the program
 void freeOpenGLProgram(GLFWwindow* window) {
-    freeShaders();
+	freeShaders();
+	glDeleteTextures(1, &tex);
 	//************Place any code here that needs to be executed once, after the main loop ends************
 }
+
+
+void texCube2(glm::mat4 P, glm::mat4 V, glm::mat4 M) {
+	//These array should rather be placed in the myCube.h file, but I placed it here so that the full solution of the exercise is placed in a single procedure
+	float myCubeTexCoords[] = {
+		1.0f, 0.0f,	  0.0f, 1.0f,    0.0f, 0.0f,
+		1.0f, 0.0f,   1.0f, 1.0f,    0.0f, 1.0f,
+
+		1.0f, 0.0f,	  0.0f, 1.0f,    0.0f, 0.0f,
+		1.0f, 0.0f,   1.0f, 1.0f,    0.0f, 1.0f,
+
+		1.0f, 0.0f,	  0.0f, 1.0f,    0.0f, 0.0f,
+		1.0f, 0.0f,   1.0f, 1.0f,    0.0f, 1.0f,
+
+		1.0f, 0.0f,	  0.0f, 1.0f,    0.0f, 0.0f,
+		1.0f, 0.0f,   1.0f, 1.0f,    0.0f, 1.0f,
+
+		1.0f, 0.0f,	  0.0f, 1.0f,    0.0f, 0.0f,
+		1.0f, 0.0f,   1.0f, 1.0f,    0.0f, 1.0f,
+
+		1.0f, 0.0f,	  0.0f, 1.0f,    0.0f, 0.0f,
+		1.0f, 0.0f,   1.0f, 1.0f,    0.0f, 1.0f,
+	};
+
+	float myCubeNormals[] = {
+		0,0,-1,0, 0,0,-1,0, 0,0,-1,0,
+		0,0,-1,0, 0,0,-1,0, 0,0,-1,0,
+
+		0,0, 1,0, 0,0, 1,0, 0,0, 1,0,
+		0,0, 1,0, 0,0, 1,0, 0,0, 1,0,
+
+		0,-1,0,0, 0,-1,0,0, 0,-1,0,0,
+		0,-1,0,0, 0,-1,0,0, 0,-1,0,0,
+
+		0, 1,0,0, 0, 1,0,0, 0, 1,0,0,
+		0, 1,0,0, 0, 1,0,0, 0, 1,0,0,
+
+		-1,0,0,0, -1,0,0,0, -1,0,0,0,
+		-1,0,0,0, -1,0,0,0, -1,0,0,0,
+
+		1,0,0,0, 1,0,0,0, 1,0,0,0,
+		1,0,0,0, 1,0,0,0, 1,0,0,0,
+	};
+
+	spLambertTextured->use();
+
+	glUniformMatrix4fv(spLambertTextured->u("P"), 1, false, glm::value_ptr(P));
+	glUniformMatrix4fv(spLambertTextured->u("V"), 1, false, glm::value_ptr(V));
+	glUniformMatrix4fv(spLambertTextured->u("M"), 1, false, glm::value_ptr(M));
+
+
+	glEnableVertexAttribArray(spLambertTextured->a("vertex"));
+	glVertexAttribPointer(spLambertTextured->a("vertex"), 4, GL_FLOAT, false, 0, verts.data());
+
+	glEnableVertexAttribArray(spLambertTextured->a("texCoord"));
+	glVertexAttribPointer(spLambertTextured->a("texCoord"), 2, GL_FLOAT, false, 0, texCoords.data());
+
+	glEnableVertexAttribArray(spLambertTextured->a("normal"));
+	glVertexAttribPointer(spLambertTextured->a("normal"), 4, GL_FLOAT, false, 0, norms.data());
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glUniform1i(spLambertTextured->u("tex"), 0);
+
+	//glDrawArrays(GL_TRIANGLES, 0, myCubeVertexCount);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, indices.data());
+
+	glDisableVertexAttribArray(spLambertTextured->a("vertex"));
+	glDisableVertexAttribArray(spLambertTextured->a("color"));
+	glDisableVertexAttribArray(spLambertTextured->a("normal"));
+}
+
 
 //Drawing procedure
 void drawScene(GLFWwindow* window, float angle_x, float angle_y) {
 	//************Place any code here that draws something inside the window******************l
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear color and depth buffers
-	
+
 	glm::mat4 M = glm::mat4(1.0f); //Initialize model matrix with abn identity matrix
 	M = glm::rotate(M, angle_y, glm::vec3(0.0f, 1.0f, 0.0f)); //Multiply model matrix by the rotation matrix around Y axis by angle_y degrees
 	M = glm::rotate(M, angle_x, glm::vec3(1.0f, 0.0f, 0.0f)); //Multiply model matrix by the rotation matrix around X axis by angle_x degrees
 	glm::mat4 V = glm::lookAt(glm::vec3(0.0f, 0.0f, -5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)); //Compute view matrix
 	glm::mat4 P = glm::perspective(glm::radians(50.0f), 1.0f, 1.0f, 50.0f); //Compute projection matrix
 
-	//Replace the lines below with your own code for drawing objects (everything related to glDrawArrays/glDrawElements)
-	//-----------
-	spLambert->use(); //Activate shader program
-	glUniform4f(spLambert->u("color"), 0, 1, 0, 1); //Set drawing color
-	glUniformMatrix4fv(spLambert->u("P"), 1, false, glm::value_ptr(P)); //Load projection matrix into the shader program
-	glUniformMatrix4fv(spLambert->u("V"), 1, false, glm::value_ptr(V)); //Load view matrix into the shader program
-	glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(M)); //Load model matrix into the shader program
-	
-	Models::torus.drawSolid(); //Narysuj obiekt
-	//-----------
+
+	texCube2(P, V, M);
 
 	glfwSwapBuffers(window); //Copy back buffer to the front buffer
 }
@@ -117,7 +248,7 @@ int main(void)
 
 	if (!glfwInit()) { //Initialize GLFW library
 		fprintf(stderr, "Can't initialize GLFW.\n");
-		exit(EXIT_FAILURE); 
+		exit(EXIT_FAILURE);
 	}
 
 	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);  //Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it. 
@@ -132,7 +263,7 @@ int main(void)
 	glfwSwapInterval(1); //During vsync wait for the first refresh
 
 	GLenum err;
-	if ((err=glewInit()) != GLEW_OK) { //Initialize GLEW library
+	if ((err = glewInit()) != GLEW_OK) { //Initialize GLEW library
 		fprintf(stderr, "Can't initialize GLEW: %s\n", glewGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
@@ -140,15 +271,15 @@ int main(void)
 	initOpenGLProgram(window); //Call initialization procedure
 
 	//Main application loop
-    float angle_x = 0; //declare variable for storing current rotation angle
+	float angle_x = 0; //declare variable for storing current rotation angle
 	float angle_y = 0; //declare variable for storing current rotation angle
 	glfwSetTime(0); //clear internal timer
 	while (!glfwWindowShouldClose(window)) //As long as the window shouldnt be closed yet...
-	{		
-        angle_x += speed_x * glfwGetTime(); //Compute an angle by which the object was rotated during the previous frame
+	{
+		angle_x += speed_x * glfwGetTime(); //Compute an angle by which the object was rotated during the previous frame
 		angle_y += speed_y * glfwGetTime(); //Compute an angle by which the object was rotated during the previous frame
 		glfwSetTime(0); //clear internal timer
-		drawScene(window,angle_x,angle_y); //Execute drawing procedure
+		drawScene(window, angle_x, angle_y); //Execute drawing procedure
 		glfwPollEvents(); //Process callback procedures corresponding to the events that took place up to now
 	}
 	freeOpenGLProgram(window);
